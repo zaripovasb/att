f939099d866796d6390e6b3f3bcd75482ca2edf9
Kakimzhanova Kyz-Zhibek <z.kakimzhanova@mail.ru>
2018-07-01 10:13:07 +0600

    add users_helper ip checks

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 	def get_user_location
-		remote_ip = request.remote_ip
-		location = Geokit::Geocoders::MultiGeocoder.geocode(remote_ip)
+		@remote_ip = request.remote_ip
+		location = Geokit::Geocoders::MultiGeocoder.geocode("66.102.9.128")
 		#@user_location = Geokit::LatLng.create(params[:lat], params[:lang])
 	end
 	def get_user_ip
